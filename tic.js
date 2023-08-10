047f05a580fcbc42c65d61023d531e3dfe7bb85a
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-08-11 00:47:29 +0300

    Add files via upload

---
+++ tic.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu");
+let aside = document.getElementById("aside");
+let fds = document.getElementById("fds")
+
+menu.onclick=()=>{
+    aside.classList.toggle("active")
+}
